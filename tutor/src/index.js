cb9338c4162aed7713c9e943bd4bf3abf36c1a94
stevenfox <stevenfoxmyk@gmail.com>
2019-07-29 16:11:35 +0100

    commit

---
+++ tutor/src/index.js
@@ -0,0 +1,14 @@
+import _ from 'lodash';
+import 'styles/app.css';
+import 'styles/appStyles.scss';
+
+function component() {
+  var element = document.createElement('div');
+
+  // Lodash, currently included via a script, is required for this line to work
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  return element;
+}
+
+document.body.appendChild(component());
